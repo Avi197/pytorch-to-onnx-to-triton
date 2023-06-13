deba65cd9e15be7920ec7ed4c1d3f9efc0aaff92
pham son <son.phhamhong2@vndirect.com.vn>
2023-06-13 14:44:07 +0700

    clean up function

--- export_yolo_to_onnx.py
+++ export_yolo_to_onnx.py
@@ -1,58 +1,58 @@
 from pathlib import Path
 import onnxruntime_extensions
+from YOLO.add_pre_post_processing_to_model import yolo_detection
 
 
-def get_yolov8_model(onnx_model_name: str):
-    # install yolov8
+def yolov8_to_onnx(yolo_path, onnx_path):
     from pip._internal import main as pipmain
     try:
         import ultralytics
     except ImportError:
         pipmain(['install', 'ultralytics'])
         import ultralytics
-    pt_model = Path("/home/hiepnt/Projects/yolov8/runs/detect/yolov8n_pti_quality2/weights_test/best.pt")
-    model = ultralytics.YOLO(str(pt_model))  # load a pretrained model
-    success = model.export(format="onnx")  # export the model to ONNX format
-    assert success, "Failed to export yolov8n.pt to onnx"
+    pt_model = Path(yolo_path)
+    model = ultralytics.YOLO(str(pt_model))
+    success = model.export(format='onnx')
+    assert success, "Failed to export to onnx"
     import shutil
-    shutil.move(pt_model.with_suffix('.onnx'), onnx_model_name)
+    shutil.move(pt_model.with_suffix('.onnx'), onnx_path)
 
 
-def add_pre_post_processing_to_yolo(input_model_file: Path, output_model_file: Path):
-    """Construct the pipeline for an end2end model with pre and post processing. 
-    The final model can take raw image binary as inputs and output the result in raw image file.
-
-    Args:
-        input_model_file (Path): The onnx yolo model.
-        output_model_file (Path): where to save the final onnx model.
-    """
-    if not Path(input_model_file).is_file():
-        get_yolov8_model(input_model_file)
-
-    from onnxruntime_extensions.tools import add_pre_post_processing_to_model as add_ppp
-    add_ppp.yolo_detection(input_model_file, output_model_file, "jpg", num_classes=3, onnx_opset=18)
+def add_pre_post_processing(yolo_path, onnx_path, onnx_after):
+    onnx_path = Path(onnx_path)
+    onnx_after = Path(onnx_after)
+    yolov8_to_onnx(yolo_path, onnx_path)
+    yolo_detection(onnx_path, onnx_after, 'jpg', num_classes=3, onnx_opset=18)
 
 
-def test_inference(onnx_model_file: Path):
+def test_inference(img_in, img_out, onnx_model_file):
     import onnxruntime as ort
     import numpy as np
-
+    onnx_model_file = Path(onnx_model_file)
     providers = ['CPUExecutionProvider']
     session_options = ort.SessionOptions()
     session_options.register_custom_ops_library(onnxruntime_extensions.get_library_path())
 
-    image = np.frombuffer(open('/opt/data/pti_ocr/quality/quality_yolo/train/images/73_0.jpg', 'rb').read(), dtype=np.uint8)
+    image = np.frombuffer(open(img_in, 'rb').read(),
+                          dtype=np.uint8)
     session = ort.InferenceSession(str(onnx_model_file), providers=providers, sess_options=session_options)
 
     inname = [i.name for i in session.get_inputs()]
     inp = {inname[0]: image}
+    # debug = session.run(['transposed_debug'], inp)[0]
     outputs = session.run(['image_out'], inp)[0]
-    open('/opt/github/pytorch-to-onnx/output.jpg', 'wb').write(outputs)
+
+    open(img_out, 'wb').write(outputs)
 
 
+#
+#
 if __name__ == '__main__':
-    print("checking the model...")
-    onnx_model_name = Path("/opt/github/pytorch-to-onnx/temp/quality_yolo.onnx")
-    onnx_e2e_model_name = onnx_model_name.with_suffix(suffix=".with_pre_post_processing.onnx")
-    add_pre_post_processing_to_yolo(onnx_model_name, onnx_e2e_model_name)
-    test_inference(onnx_e2e_model_name)
+    yolo_path = '/opt/github/pytorch-to-onnx/best.pt'
+    onnx_path = '/opt/github/pytorch-to-onnx/models/quality_onnx.onnx'
+    onnx_after = '/opt/github/pytorch-to-onnx/models/quality_onnx_pre_post.onnx'
+    add_pre_post_processing(yolo_path, onnx_path, onnx_after)
+
+    img_test = '/opt/data/pti_ocr/quality/quality_yolo2/images/train/73_0.jpg'
+    img_out = '/opt/github/pytorch-to-onnx/output.jpg'
+    test_inference(img_test, img_out, onnx_after)
--- yolo_test.py
+++ yolo_test.py
@@ -2,7 +2,7 @@ import cv2
 from ultralytics import YOLO
 
 # Load the YOLOv8 model
-model = YOLO('/home/hiepnt/Projects/yolov8/runs/detect/yolov8n_pti_quality2/weights_test/best.pt')
+model = YOLO('/opt/github/pytorch-to-onnx/temp/quality_yolo.onnx')
 
 # results = self.model.predict(img, stream=False)                 # run prediction on img
 # for result in results:                                         # iterate results
@@ -11,7 +11,7 @@ model = YOLO('/home/hiepnt/Projects/yolov8/runs/detect/yolov8n_pti_quality2/weig
 #         r = box.xyxy[0].astype(int)                            # get corner points as int
 #         print(r)                                               # print boxes
 #         cv2.rectangle(img, r[:2], r[2:], (255, 255, 255), 2)   # draw boxes on img
-img_path = '/opt/github/pytorch-to-onnx/image_1686301058.png'
+img_path = '/opt/data/pti_ocr/quality/quality_yolo2/images/train/73_0.jpg'
 
 model.predict(img_path, save=True, imgsz=640, conf=0.5)
 
